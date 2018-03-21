26721d32887be40ba2aac35e4e96ee8fee6889f9
LSLIM <LSLIM@LS>
2018-03-21 22:08:10 +0100

    I replace Thread.Sleep(4000) into IsBackground = true

--- MultiThreading/Program.cs
+++ MultiThreading/Program.cs
@@ -12,6 +12,8 @@ namespace MultiThreading
         {
             Thread obj1 = new Thread(Function1);
             Thread obj2 = new Thread(Function2);
+            obj1.IsBackground = true;
+            obj2.IsBackground = true;
             obj1.Start();
             obj2.Start();
             Console.ReadLine();
@@ -22,19 +24,17 @@ namespace MultiThreading
     {
         static void Function1()
         {
-            for(int i = 0; i < 10; i++)
+            for (int i = 0; i <= 10; i++)
             {
                 Console.WriteLine("Function i excuted " + i.ToString());
-                Thread.Sleep(4000);
             }
         }
 
         static void Function2()
         {
-            for(int j = 0; j < 10; j++)
+            for (int j = 0; j <= 10; j++)
             {
                 Console.WriteLine("Function j excuted " + j.ToString());
-                Thread.Sleep(4000);
             }
         }
     }
