70a76164da4e1c7d5d06bc794277a45b8533d9dd
LSLIM <LSLIM@LS>
2018-03-21 21:33:56 +0100

    I add a new class ThreadProgram

--- MultiThreading/Program.cs
+++ MultiThreading/Program.cs
@@ -2,14 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace MultiThreading
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Thread obj1 = new Thread(Function1);
+            Thread obj2 = new Thread(Function2);
+            obj1.Start();
+            obj2.Start();
+            Console.ReadLine();
+        }
+    }
+
+    class ThreadProgram
+    {
+        static void Function1()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Function 1 excuted " + i.ToString());
+                Thread.Sleep(4000);
+            }
+        }
+
+        static void Function2()
+        {
+            for(int j = 0; j < 10; j++)
+            {
+                Console.WriteLine("Function j excuted " + j.ToString());
+                Thread.Sleep(4000);
+            }
         }
     }
 }
